513bff24a111eff2272ee58c12e206809e55377e
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-07-28 20:29:32 +0100

    basic navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
+import './App.css';
+import Lashies from './assets/lash.jpg';
+import Navigation from './components/Navigation';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <header>
+          <img src={Lashies} alt="eyelashes"/>
+          <Navigation />
+        </header>
         <p>heelo</p>
       </div>
     );
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <a>Home</a>
+            <a>Prices</a>
+            <a>About Me</a>
+            <a>Contact</a>
+        </nav>
+    )
+};
+
+export default Navigation;
