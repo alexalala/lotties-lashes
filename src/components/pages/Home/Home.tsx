b1744fcc6c4117396ae6d465ed2881c843fb8f42
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-09-19 22:25:39 +0200

    home page scoped out and image and banner styled

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react';
 
+import LashPic from '../../../assets/lash-prep.png';
+import * as HomeCopy from '../../../content/home.json';
+import './Home.css';
+
 const Home: React.FunctionComponent = () =>
-<p>Home page</p>
+    <div>
+        <div className="headline-banner">
+            <div className="image-container">
+                <img src={LashPic} alt="eyelash process picture" />
+                <div className="image-overlay" />
+            </div>
+            <div>
+                <p>{HomeCopy.inspoQuote}</p>
+            </div>
+        </div>
+    </div>
 
 export default Home;
