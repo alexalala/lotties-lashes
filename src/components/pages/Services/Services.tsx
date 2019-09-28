ae4be4e1269c40e5a39e17a590e7d5171cff90e5
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-09-28 19:36:53 +0100

    services page and menu outline styled

--- src/components/pages/Services/Services.tsx
+++ src/components/pages/Services/Services.tsx
@@ -1,6 +1,23 @@
 import * as React from 'react';
+import * as ServicesCopy from '../../../content/services.json';
+
+import './Services.css';
 
 const Services: React.FunctionComponent = () =>
-<p>Services page</p>
+<div className="services-container-outer">
+    <div>
+        <div>
+            <h2>{ServicesCopy.title}</h2>
+            {
+                ServicesCopy.services.map((service, k) =>
+                    <div key={k}>
+                        <h3>{service.title} - £{service.price}</h3>
+                        <p>{service.description}</p>
+                    </div>
+                )
+            }
+        </div>
+    </div>
+</div>
 
 export default Services;
