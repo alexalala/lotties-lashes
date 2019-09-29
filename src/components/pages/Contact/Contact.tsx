47c45b5111ab3030ed659fc9dfa61b1829f260c6
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-09-29 19:37:10 +0100

    printed all copy to the page

--- src/components/pages/Contact/Contact.tsx
+++ src/components/pages/Contact/Contact.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import ImageWithBorder from '../../ImageWithBorder/ImageWithBorder';
 
+import * as ContactCopy from '../../../content/contact.json';
+
 import LashPic from '../../../assets/lash-prep.png';
 
 import './Contact.css';
@@ -9,6 +11,25 @@ import './Contact.css';
 const Contact: React.FunctionComponent = () =>
     <div className="contact">
         <ImageWithBorder source={LashPic} />
+        <div>
+            <div>
+                <h1>{ContactCopy.title}</h1>
+                <p>{ContactCopy.description}</p>
+                <div>
+                    <p>Hours:</p>
+                    <div>
+                        {
+                            Object.keys(ContactCopy.hours).map((key, i) => (
+                                <div key={i}>
+                                    <p>{key}</p>
+                                    <p>{ContactCopy.hours[key]}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 export default Contact;
